f555e9c7dc306bf0f64436f2e365b223c86604f8
jazael ramos <borch_sk8@hotmail.com>
2026-05-27 15:39:38 -0600

    programa inicial de sequential Mandelbrot implementation

---
+++ mandelbrot_secuencial.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <complex>
+#include <chrono>
+
+using namespace std;
+
+const int WIDTH = 7680;   // 8K
+const int HEIGHT = 4320;
+const int MAX_ITER = 1000;
+
+struct Pixel {
+    unsigned char r, g, b;
+};
+
+int main() {
+
+    auto inicio = chrono::high_resolution_clock::now();
+
+    vector<Pixel> image(WIDTH * HEIGHT);
+
+    for(int y = 0; y < HEIGHT; y++) {
+
+        for(int x = 0; x < WIDTH; x++) {
+
+            double cx = (x - WIDTH/2.0) * 4.0 / WIDTH;
+            double cy = (y - HEIGHT/2.0) * 4.0 / WIDTH;
+
+            complex<double> c(cx, cy);
+            complex<double> z(0,0);
+
+            int iter = 0;
+
+            while(abs(z) < 2.0 && iter < MAX_ITER) {
+                z = z*z + c;
+                iter++;
+            }
+
+            Pixel p;
+
+            if(iter == MAX_ITER) {
+                p.r = 0;
+                p.g = 0;
+                p.b = 0;
+            }
+            else {
+                p.r = iter % 256;
+                p.g = (iter * 5) % 256;
+                p.b = (iter * 10) % 256;
+            }
+
+            image[y*WIDTH + x] = p;
+        }
+    }
+
+    ofstream file("mandelbrot.ppm", ios::binary);
+
+    file << "P6\n";
+    file << WIDTH << " " << HEIGHT << "\n255\n";
+
+    for(const auto &p : image) {
+        file.write((char*)&p, 3);
+    }
+
+    file.close();
+
+    auto fin = chrono::high_resolution_clock::now();
+
+    chrono::duration<double> tiempo = fin - inicio;
+
+    cout << "Tiempo secuencial: "
+         << tiempo.count()
+         << " segundos\n";
+
+    return 0;
+}
+
